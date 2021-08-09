9739edb8d1cd13487bdf9391fca166dfc77c9e8d
TDCIAN <tdcian71@gmail.com>
2021-08-09 12:05:22 +0900

    Initial Commit

---
+++ PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by 김정민 on 2021/08/09.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
